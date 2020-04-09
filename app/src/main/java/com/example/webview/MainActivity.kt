a9db3c88087d2fe9d5307348fd6a8d12e0563f74
Tanwar <shweta_tanwar@ymail.com>
2020-04-09 11:02:49 +0530

    Corrected url typo error

--- app/src/main/java/com/example/webview/MainActivity.kt
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.webview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webView.loadUrl("https://www.kalatmak.com/")
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://www.kalatmak.co.in/")
     }
 }
